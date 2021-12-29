b6b0d4885155e203edc1ef160df42d4e3329101d
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2021-12-29 17:51:02 +0900

    Delete 완주하지 못한 선수.py

--- 프로그래머스/완주하지 못한 선수.py
+++
@@ -1,8 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    
-    for idx, person in enumerate(completion):
-        if person != participant[idx]:
-            return participant[idx]
-    return participant[-1]
