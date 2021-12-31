a4155856c833ed8b067d71d1f1677bc9fa552cf3
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2021-12-31 19:37:47 +0900

    Create 124나라의 숫자.py

---
+++ 프로그래머스/LV2/124나라의 숫자.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = ''
+    while n:
+        if str(n % 3) == '0':
+            answer = '4' + answer
+            n = n // 3 - 1
+        else:
+            answer = str(n % 3) + answer
+            n //= 3
+    
+    return answer
